d327be71e8f2cab9f5c1e1d3e08573473fe746a3
Gabriel Cervantes <gcervantes@technisupport.com>
2021-12-04 22:23:10 -0500

    rutas vacaciones

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const usuarios = require('./data_user');
+let vacaciones_req = require('./data_vacaciones');
 
 app.use(cors());
 
@@ -72,8 +73,8 @@ app.get('/user_profile/:email', (req, res) => {
  * Descripcion: Permite editar al usuario por medio del id
  * Ruta: /user_edit
  * Metodo: POST
- * Datos entrada: {email, paswword}
- * Respuesta: {  msg, status, user: user_find }
+ * Datos entrada: {address, phone, id}
+ * Respuesta: {  { msg: 'Usuario editado', status: 200, usuario: usuarios[i] }}
  */
 app.post('/user_edit', (req, res) => {
   const { address, phone, id } = req.body;
@@ -110,6 +111,83 @@ app.post('/user_edit', (req, res) => {
   found
     ? res.send({ msg: 'Usuario editado', status: 200, usuario: usuarios[i] })
     : res.send({ msg: 'Usuario no encontrado', status: 400, usuario: null });
+});
+
+/**
+ * API Rest Solicitud vacaciones
+ * Descripcion: Permite solicitar al usuario por medio del id las vacaciones
+ * Ruta: /vacations_request
+ * Metodo: POST
+ * Datos entrada: {id, init_date, final_date}
+ * Respuesta: { msg, status, solicitud: new_vac_req }
+ */
+
+app.post('/vacations_request', (req, res) => {
+  const { id, init_date, final_date } = req.body;
+
+  const id_valid = usuarios.find((us) => us.id === id);
+
+  let msg = '';
+  let status = '';
+
+  if (!id_valid) {
+    msg = 'usuario no valido';
+    status = 400;
+    return res.send({ msg, status, solicitud: null });
+  }
+
+  const new_vac_req = {
+    id: `v-${Date.now()}`,
+    id_user: id,
+    estado: 'Pendiente',
+    fecha_ini: init_date,
+    fecha_fin: final_date,
+  };
+
+  vacaciones_req.push(new_vac_req);
+
+  msg = 'Solicitud guarada';
+  status = 200;
+
+  res.send({ msg, status, solicitud: new_vac_req });
+});
+
+/**
+ * API Rest Solicitud vacaciones
+ * Descripcion: Permite solicitar al usuario por medio del id las vacaciones
+ * Ruta: /vacations_request
+ * Metodo: POST
+ * Datos entrada: {id, init_date, final_date}
+ * Respuesta: { msg, status, solicitud: new_vac_req }
+ */
+app.post('/vacations_response', (req, res) => {
+  const { id, vac_res } = req.body;
+
+  let msg = '';
+  let status = '';
+
+  const vac_req_found = vacaciones_req.find((vac) => vac.id === id);
+
+  console.log(vac_req_found);
+
+  if (!vac_req_found) {
+    msg = 'Solicitud no valida';
+    status = 400;
+    return res.send({ msg, status, solicitud: null });
+  }
+
+  const vac_update = vacaciones_req.map((vac) => {
+    if (vac.id === vac_req_found.id) {
+      vac.estado = vac_res;
+      return vac;
+    } else {
+      return vac;
+    }
+  });
+
+  vacaciones_req = [...vac_update];
+
+  res.send({ msg, status, vacaciones_req });
 });
 
 module.exports = app;
---
+++ src/data_vacaciones.js
@@ -0,0 +1,18 @@
+const vacaciones_req = [
+  {
+    id: 'v-123',
+    id_user: '123',
+    estado: 'Aceptada',
+    fecha_ini: '2021-12-1',
+    fecha_fin: '2021-12-15',
+  },
+  {
+    id: 'v-456',
+    id_user: '456',
+    estado: 'Pendiente',
+    fecha_ini: '2022-1-5',
+    fecha_fin: '2022-1-20',
+  },
+];
+
+module.exports = vacaciones_req;
